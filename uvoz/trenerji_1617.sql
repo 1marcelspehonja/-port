070c0e03e39255f17ca12f412b1e48f68e990c60
Marcel Spehonja <spehonja.marcel@gmail.com>
2020-05-13 22:01:37 +0200

    ogrodje za trenerje

---
+++ uvoz/trenerji_1617.sql
@@ -0,0 +1,2 @@
+INSERT INTO trenerji_1617(TRENER,EKIPA,st_let_s_klubom,st_let_kariera,G_sezona,W_sezona,L_sezona,G_s_klubom,W_s_klubom,L_s_klubom,G_kariera,W_kariera,L_kariera,W_pr) VALUES ('Mike Budenholzer','ATL',4,4,82,43,39,328,189,139,328,189,139,.576);
+
